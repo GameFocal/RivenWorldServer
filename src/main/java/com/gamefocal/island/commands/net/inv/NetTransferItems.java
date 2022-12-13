e625c317c05d3b262609def2a48e68d17dd41795
Zackary Pedersen <zackary@snaju.com>
2022-12-13 13:19:54 -0600

    Got transfer working, and now need to work on split stack logic

--- src/main/java/com/gamefocal/island/commands/net/inv/NetTransferItems.java
+++ src/main/java/com/gamefocal/island/commands/net/inv/NetTransferItems.java
@@ -3,6 +3,7 @@ package com.gamefocal.island.commands.net.inv;
 import com.gamefocal.island.DedicatedServer;
 import com.gamefocal.island.entites.net.*;
 import com.gamefocal.island.game.inventory.Inventory;
+import com.gamefocal.island.game.inventory.InventoryItem;
 import com.gamefocal.island.game.inventory.InventoryStack;
 import com.gamefocal.island.game.tasks.seqence.ExecSequenceAction;
 import com.gamefocal.island.game.tasks.seqence.WaitSequenceAction;
@@ -20,48 +21,98 @@ public class NetTransferItems extends HiveCommand {
 
         System.out.println(message);
 
-//        String fromInventoryId = message.args[0];
-//        String toInventoryId = message.args[1];
-//
-//        Inventory from = DedicatedServer.get(InventoryService.class).getInvFromId(UUID.fromString(fromInventoryId));
-//        Inventory to = DedicatedServer.get(InventoryService.class).getInvFromId(UUID.fromString(toInventoryId));
-//
-//        if (from != null && to != null && !fromInventoryId.equalsIgnoreCase(toInventoryId)) {
-//            // Are valid inventories
-//
-//            if (from.isOwner(netConnection) && to.isOwner(netConnection)) {
-//                // Owner of both
-//
-//                if(to.isLocked()) {
-//                    return;
-//                }
-//
-//                int i = 0;
-//                for (InventoryStack stack : from.getItems()) {
-//                    if (to.canAdd(stack)) {
-//                        to.add(stack);
-//                        from.clear(i);
-//                    }
-//                    i++;
-//                }
-//
-//                to.update();
-//                from.update();
-//
-//                TaskService.scheduleTaskSequence(false, new ExecSequenceAction() {
-//                    @Override
-//                    public void run() {
-//                        netConnection.updateInventory(to);
-//                    }
-//                }, new WaitSequenceAction(1L), new ExecSequenceAction() {
-//                    @Override
-//                    public void run() {
-//                        netConnection.updateInventory(from);
-//                    }
-//                });
-//            }
-//
-//        }
-//
+        String type = message.args[0];
+        String objSlug = message.args[1];
+        int customAmt = Integer.parseInt(message.args[2]);
+        String fromId = message.args[3];
+        String toId = message.args[4];
+        int fromSlot = Integer.parseInt(message.args[5]);
+
+        Inventory from = DedicatedServer.get(InventoryService.class).getInvFromId(UUID.fromString(fromId));
+        Inventory to = DedicatedServer.get(InventoryService.class).getInvFromId(UUID.fromString(toId));
+
+        if (from != null && to != null && !toId.equalsIgnoreCase(fromId)) {
+            // Are valid inventories
+
+            if (from.isOwner(netConnection) && to.isOwner(netConnection)) {
+                // Owner of both
+
+                if (to.isLocked()) {
+                    return;
+                }
+
+                if (type.equalsIgnoreCase("Transfer One")) {
+                    // Transfer One
+
+                    InventoryStack fromStack = from.get(fromSlot);
+                    if (fromStack != null && fromStack.getAmount() >= 1) {
+                        if (to.canAdd(new InventoryStack(fromStack.getItem(), 1))) {
+                            to.add(fromStack.getItem(), 1);
+                            fromStack.remove(1);
+                        }
+                    }
+
+                } else if (type.equalsIgnoreCase("Transfer Stack")) {
+                    // Transfer all of stack
+
+                    InventoryStack fromStack = from.get(fromSlot);
+                    if (fromStack != null) {
+                        if (to.canAdd(fromStack)) {
+                            to.add(fromStack);
+                            from.clear(fromSlot);
+                        }
+                    }
+
+                } else if (type.equalsIgnoreCase("Transfer all of type")) {
+                    // Transfer all of this type
+
+                    Class<? extends InventoryItem> itemClass = DedicatedServer.get(InventoryService.class).getItemClassFromSlug(objSlug);
+                    if (itemClass != null) {
+
+                        for (InventoryStack s : from.getOfType(itemClass)) {
+                            // Attempt to transfer this
+                            if (to.canAdd(s)) {
+                                to.add(s);
+                                s.clear();
+                            }
+                        }
+
+                    }
+
+                } else if (type.equalsIgnoreCase("Transfer Custom")) {
+                    // Transfer Custom Amt
+
+                    InventoryStack fromStack = from.get(fromSlot);
+                    if (fromStack != null && fromStack.getAmount() >= customAmt) {
+                        if (to.canAdd(new InventoryStack(fromStack.getItem(), customAmt))) {
+                            to.add(fromStack.getItem(), customAmt);
+                            fromStack.remove(customAmt);
+                        }
+                    }
+
+                }
+
+                /*
+                 * Update the inventory here
+                 * */
+                to.update();
+                from.update();
+
+                TaskService.scheduleTaskSequence(false, new ExecSequenceAction() {
+                    @Override
+                    public void run() {
+                        netConnection.updateInventory(to);
+                    }
+                }, new WaitSequenceAction(1L), new ExecSequenceAction() {
+                    @Override
+                    public void run() {
+                        netConnection.updateInventory(from);
+                    }
+                });
+
+            }
+
+        }
+
     }
 }
