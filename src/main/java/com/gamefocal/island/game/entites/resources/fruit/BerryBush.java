f8a3b6c42835ec2dfdf581568fdf60c9e3302e5f
Zackary Pedersen <zackary@snaju.com>
2023-02-01 17:15:09 -0600

    Working on world stuff :)

--- src/main/java/com/gamefocal/island/game/entites/resources/fruit/BerryBush.java
+++ src/main/java/com/gamefocal/island/game/entites/resources/fruit/BerryBush.java
@@ -1,12 +1,18 @@
 package com.gamefocal.island.game.entites.resources.fruit;
 
+import com.gamefocal.island.DedicatedServer;
 import com.gamefocal.island.entites.net.HiveNetConnection;
 import com.gamefocal.island.game.GameEntity;
 import com.gamefocal.island.game.InteractableEntity;
 import com.gamefocal.island.game.interactable.InteractAction;
 import com.gamefocal.island.game.inventory.InventoryStack;
+import com.gamefocal.island.game.items.food.consumable.Blueberry;
+import com.gamefocal.island.game.sounds.GameSounds;
+import com.gamefocal.island.game.util.RandomUtil;
 
 public class BerryBush extends GameEntity<BerryBush> implements InteractableEntity {
+
+    public boolean hasBerries = true;
 
     public BerryBush() {
         this.type = "berry-bush";
@@ -23,12 +29,32 @@ public class BerryBush extends GameEntity<BerryBush> implements InteractableEnti
     }
 
     @Override
-    public void onTick() {
+    public void onSync() {
+        this.setMeta("fruit", this.hasBerries);
+    }
 
+    @Override
+    public void onTick() {
+//        this.setMeta("fruit",this.hasBerries);
     }
 
     @Override
     public void onInteract(HiveNetConnection connection, InteractAction action, InventoryStack inHand) {
-        System.out.println(action);
+        if (action == InteractAction.USE) {
+            if (this.hasBerries) {
+                int amt = RandomUtil.getRandomNumberBetween(3, 8);
+
+                connection.playLocalSoundAtLocation(GameSounds.FORAGE_TREE, this.location, .75f, 1);
+
+                InventoryStack stack = new InventoryStack(new Blueberry(), amt);
+
+                connection.getPlayer().inventory.add(stack);
+                connection.displayItemAdded(stack);
+
+                DedicatedServer.instance.getWorld().updateEntity(this);
+
+                this.hasBerries = false;
+            }
+        }
     }
 }
--- src/main/java/com/gamefocal/island/game/entites/resources/ground/SmallRockEntity.java
+++ src/main/java/com/gamefocal/island/game/entites/resources/ground/SmallRockEntity.java
@@ -1,10 +1,13 @@
 package com.gamefocal.island.game.entites.resources.ground;
 
+import com.gamefocal.island.DedicatedServer;
 import com.gamefocal.island.entites.net.HiveNetConnection;
 import com.gamefocal.island.game.GameEntity;
 import com.gamefocal.island.game.InteractableEntity;
 import com.gamefocal.island.game.interactable.InteractAction;
 import com.gamefocal.island.game.inventory.InventoryStack;
+import com.gamefocal.island.game.items.resources.minerals.raw.Stone;
+import com.gamefocal.island.game.items.resources.wood.WoodStick;
 
 public class SmallRockEntity extends GameEntity<SmallRockEntity> implements InteractableEntity {
 
@@ -29,6 +32,13 @@ public class SmallRockEntity extends GameEntity<SmallRockEntity> implements Inte
 
     @Override
     public void onInteract(HiveNetConnection connection, InteractAction action, InventoryStack inHand) {
-        System.out.println(action);
+        if (action == InteractAction.USE) {
+            // Pickup the Stick
+
+            InventoryStack stack = new InventoryStack(new Stone());
+            connection.displayItemAdded(stack);
+            connection.getPlayer().inventory.add(stack);
+            DedicatedServer.instance.getWorld().despawn(this.uuid);
+        }
     }
 }
