12ddf060935726f291b5eb156e221983630b724b
Zackary Pedersen <zackary@snaju.com>
2023-02-02 16:25:40 -0600

    Fixed doors

---
+++ src/main/java/com/gamefocal/island/game/entites/generics/DoorEntity.java
@@ -0,0 +1,64 @@
+package com.gamefocal.island.game.entites.generics;
+
+import com.gamefocal.island.DedicatedServer;
+import com.gamefocal.island.entites.net.HiveNetConnection;
+import com.gamefocal.island.game.InteractableEntity;
+import com.gamefocal.island.game.entites.placable.PlaceableEntity;
+import com.gamefocal.island.game.interactable.InteractAction;
+import com.gamefocal.island.game.inventory.InventoryItem;
+import com.gamefocal.island.game.inventory.InventoryStack;
+
+public abstract class DoorEntity<T> extends PlaceableEntity<T> implements InteractableEntity {
+
+    protected InventoryItem lock;
+
+    protected boolean isOpen = false;
+
+    @Override
+    public void onSpawn() {
+
+    }
+
+    @Override
+    public void onSync() {
+        this.setMeta("open", (this.isOpen ? "t" : "f"));
+    }
+
+    @Override
+    public void onDespawn() {
+
+    }
+
+    @Override
+    public void onTick() {
+
+    }
+
+    public InventoryItem getLock() {
+        return lock;
+    }
+
+    public void setLock(InventoryItem lock) {
+        this.lock = lock;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    @Override
+    public void onInteract(HiveNetConnection connection, InteractAction action, InventoryStack inHand) {
+        super.onInteract(connection, action, inHand);
+        if (action == InteractAction.USE) {
+            // Open the door
+            // TODO: Do a permission check based on lock here.
+            this.isOpen = !this.isOpen;
+
+            DedicatedServer.instance.getWorld().updateEntity(this);
+        }
+    }
+}
--- src/main/java/com/gamefocal/island/game/entites/placable/DoorPlaceable.java
+++ src/main/java/com/gamefocal/island/game/entites/placable/DoorPlaceable.java
@@ -1,9 +1,13 @@
 package com.gamefocal.island.game.entites.placable;
 
-public class DoorPlaceable extends PlaceableEntity<DoorPlaceable> {
+import com.gamefocal.island.game.entites.generics.DoorEntity;
+
+public class DoorPlaceable extends DoorEntity<DoorPlaceable> {
 
     public DoorPlaceable() {
         this.type = "DoorPlaceable";
+        this.health = 200;
+        this.maxHealth = 200;
     }
 
     @Override
