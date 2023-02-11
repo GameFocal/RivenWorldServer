b89d9fc2efba831df445aca98df1d822f19bb2d8
Zackary Pedersen <zackary@snaju.com>
2023-02-11 10:48:41 -0600

    Working on net entity interaction :)

--- src/main/java/com/gamefocal/island/commands/net/player/NetPlayerAction.java
+++ src/main/java/com/gamefocal/island/commands/net/player/NetPlayerAction.java
@@ -1,6 +1,14 @@
 package com.gamefocal.island.commands.net.player;
 
 import com.gamefocal.island.entites.net.*;
+import com.gamefocal.island.game.GameEntity;
+import com.gamefocal.island.game.InteractableEntity;
+import com.gamefocal.island.game.interactable.InteractAction;
+import com.gamefocal.island.game.inventory.InventoryStack;
+import com.gamefocal.island.game.ray.HitResult;
+import com.gamefocal.island.game.ray.hit.EntityHitResult;
+import com.gamefocal.island.game.ray.hit.FoliageHitResult;
+import com.gamefocal.island.game.ray.hit.TerrainHitResult;
 
 @Command(name = "a-e", sources = "tcp")
 public class NetPlayerAction extends HiveCommand {
@@ -8,9 +16,32 @@ public class NetPlayerAction extends HiveCommand {
     public void onCommand(HiveNetMessage message, CommandSource source, HiveNetConnection netConnection) throws Exception {
 
         /*
-        * Process the Primary Action Event
-        * */
-        // TODO: Process the interact/ input here :)
+         * Process the Primary Action Event
+         * */
+        HitResult r = netConnection.getLookingAt();
+
+        if (FoliageHitResult.class.isAssignableFrom(r.getClass())) {
+            // Foliage interact
+
+            // TODO: Trigger Foliage Interaction
+
+        } else if (EntityHitResult.class.isAssignableFrom(r.getClass())) {
+            // Entity Interact
+
+            GameEntity e = (GameEntity) r.get();
+            if (InteractableEntity.class.isAssignableFrom(e.getClass())) {
+                if (((InteractableEntity) e).canInteract(netConnection)) {
+
+                    // Get Inhand
+                    InventoryStack inhand = netConnection.getPlayer().equipmentSlots.getWeapon();
+
+                    ((InteractableEntity) e).onInteract(netConnection, InteractAction.USE, inhand);
+                }
+            }
+
+        } else if(TerrainHitResult.class.isAssignableFrom(r.getClass())) {
+            // TODO: Trigger Terrain Forage
+        }
 
 
     }
