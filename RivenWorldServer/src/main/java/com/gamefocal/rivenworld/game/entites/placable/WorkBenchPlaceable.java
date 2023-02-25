e37f381ba05c56fd694ddadb992e09c4ded93ad4
Rene Herrera <rene@snaju.com>
2023-02-24 20:05:11 -0600

    workbench

--- RivenWorldServer/src/main/java/com/gamefocal/rivenworld/game/entites/placable/WorkBenchPlaceable.java
+++ RivenWorldServer/src/main/java/com/gamefocal/rivenworld/game/entites/placable/WorkBenchPlaceable.java
@@ -1,9 +1,19 @@
 package com.gamefocal.rivenworld.game.entites.placable;
 
-public class WorkBenchPlaceable extends PlaceableEntity<WorkBenchPlaceable> {
+import com.gamefocal.rivenworld.entites.net.HiveNetConnection;
+import com.gamefocal.rivenworld.game.entites.generics.EntityStorageInterface;
+import com.gamefocal.rivenworld.game.interactable.InteractAction;
+import com.gamefocal.rivenworld.game.inventory.Inventory;
+import com.gamefocal.rivenworld.game.inventory.InventoryStack;
+import com.gamefocal.rivenworld.game.inventory.InventoryType;
+import com.gamefocal.rivenworld.game.ui.inventory.CraftingInventoryUI;
 
+public class WorkBenchPlaceable extends PlaceableEntity<WorkBenchPlaceable> implements EntityStorageInterface {
+
+    protected Inventory inventory = new Inventory(InventoryType.WORKBENCH, "Workbench", "Workbench", 6);
     public WorkBenchPlaceable() {
         this.type = "workbenchPlaceable";
+        this.inventory.setAttachedEntity(this.uuid);
     }
 
     @Override
@@ -19,5 +29,33 @@ public class WorkBenchPlaceable extends PlaceableEntity<WorkBenchPlaceable> {
     @Override
     public void onTick() {
 
+    }
+
+    @Override
+    public void onInventoryUpdated() {
+
+    }
+
+    @Override
+    public void onInventoryOpen() {
+
+    }
+
+    @Override
+    public void onInteract(HiveNetConnection connection, InteractAction action, InventoryStack inHand) {
+        if (action == InteractAction.USE) {
+            CraftingInventoryUI ui = new CraftingInventoryUI();
+            ui.open(connection, this.inventory);
+        }
+    }
+
+    @Override
+    public void onInventoryClosed() {
+
+    }
+
+    @Override
+    public String onFocus(HiveNetConnection connection) {
+        return "[e] Use";
     }
 }
