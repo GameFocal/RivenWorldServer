bf39dcf879b3dd2576f129c74ae71c6e0211d68a
Rene Herrera <rene@snaju.com>
2023-04-12 11:43:45 -0500

    fix jails

--- RivenWorldServer/src/main/java/com/gamefocal/rivenworld/game/entites/placable/decoration/jail_3.java
+++ RivenWorldServer/src/main/java/com/gamefocal/rivenworld/game/entites/placable/decoration/jail_3.java
@@ -1,8 +1,9 @@
 package com.gamefocal.rivenworld.game.entites.placable.decoration;
 
+import com.gamefocal.rivenworld.game.entites.generics.DoorEntity;
 import com.gamefocal.rivenworld.game.entites.placable.PlaceableEntity;
 
-public class jail_3 extends PlaceableEntity<jail_3> {
+public class jail_3 extends DoorEntity<jail_3> {
 
     public jail_3() {
         this.type = "jail_3";
