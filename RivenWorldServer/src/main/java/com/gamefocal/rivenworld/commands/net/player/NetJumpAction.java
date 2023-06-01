b93f54adeeacad6727f24329b6e68acc4bbeee5f
Rene Herrera <rene@snaju.com>
2023-06-01 16:55:20 -0500

    add jump command

---
+++ RivenWorldServer/src/main/java/com/gamefocal/rivenworld/commands/net/player/NetJumpAction.java
@@ -0,0 +1,14 @@
+package com.gamefocal.rivenworld.commands.net.player;
+
+import com.gamefocal.rivenworld.entites.net.*;
+
+@Command(name = "JUMP", sources = "tcp")
+public class NetJumpAction extends HiveCommand {
+    @Override
+    public void onCommand(HiveNetMessage message, CommandSource source, HiveNetConnection netConnection) throws Exception {
+        if (netConnection.getPlayer().playerStats.energy > 5) {
+            netConnection.getPlayer().playerStats.energy -= 5;
+            System.out.println("got jump command");
+        }
+    }
+}
