086565b921e79905c1e997955f14d4a73b1fc6cc
MelissaHerrera <melissa@snaju.com>
2023-06-07 15:22:34 -0500

    Added target :)

---
+++ RivenWorldServer/src/main/java/com/gamefocal/rivenworld/game/items/placables/blocks/Clay/ClayHalfBlockItem.java
@@ -0,0 +1,43 @@
+package com.gamefocal.rivenworld.game.items.placables.blocks.Clay;
+
+import com.gamefocal.rivenworld.entites.net.HiveNetConnection;
+import com.gamefocal.rivenworld.game.GameEntity;
+import com.gamefocal.rivenworld.game.entites.blocks.Clay.ClayHalfBlock;
+import com.gamefocal.rivenworld.game.entites.blocks.Stone.StoneHalfBlock;
+import com.gamefocal.rivenworld.game.interactable.InteractAction;
+import com.gamefocal.rivenworld.game.interactable.Intractable;
+import com.gamefocal.rivenworld.game.inventory.CraftingRecipe;
+import com.gamefocal.rivenworld.game.inventory.InventoryCraftingInterface;
+import com.gamefocal.rivenworld.game.inventory.enums.InventoryDataRow;
+import com.gamefocal.rivenworld.game.items.generics.PlaceableInventoryItem;
+import com.gamefocal.rivenworld.game.recipes.blocks.ClayHalfBlockRecipe;
+import com.gamefocal.rivenworld.game.recipes.blocks.ClayStairsBlockRecipe;
+import com.gamefocal.rivenworld.game.recipes.blocks.StoneHalfBlockRecipe;
+
+public class ClayHalfBlockItem extends PlaceableInventoryItem<ClayHalfBlockItem> implements InventoryCraftingInterface {
+
+    public ClayHalfBlockItem() {
+        this.name = "Clay Half Block";
+        this.desc = "A half block made of clay";
+        this.icon = InventoryDataRow.Clay_HalfBlock;
+        this.mesh = InventoryDataRow.Clay_HalfBlock;
+        this.placable.IsBuildingBlock = true;
+        this.placable.HalfBlock = true;
+        this.spawnNames.add("clayhalfblock");
+    }
+
+    @Override
+    public void onInteract(Intractable intractable, HiveNetConnection connection, InteractAction action) {
+
+    }
+
+    @Override
+    public GameEntity spawnItem() {
+        return new ClayHalfBlock();
+    }
+
+    @Override
+    public CraftingRecipe canCraft(HiveNetConnection connection) {
+        return new ClayHalfBlockRecipe();
+    }
+}
