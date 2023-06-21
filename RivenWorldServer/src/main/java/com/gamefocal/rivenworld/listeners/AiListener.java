d5f5305f8e8fa19c556b671057eae54522f949d7
Zackary Pedersen <zackary@snaju.com>
2023-06-21 16:57:04 -0500

    Working on shrine :)

--- RivenWorldServer/src/main/java/com/gamefocal/rivenworld/listeners/AiListener.java
+++ RivenWorldServer/src/main/java/com/gamefocal/rivenworld/listeners/AiListener.java
@@ -3,9 +3,18 @@ package com.gamefocal.rivenworld.listeners;
 import com.gamefocal.rivenworld.DedicatedServer;
 import com.gamefocal.rivenworld.entites.events.EventHandler;
 import com.gamefocal.rivenworld.entites.events.EventInterface;
+import com.gamefocal.rivenworld.entites.net.HiveNetConnection;
+import com.gamefocal.rivenworld.events.game.ServerReadyEvent;
 import com.gamefocal.rivenworld.events.game.ServerWorldSyncEvent;
+import com.gamefocal.rivenworld.events.world.SundownEvent;
+import com.gamefocal.rivenworld.events.world.SunriseEvent;
+import com.gamefocal.rivenworld.game.GameEntity;
 import com.gamefocal.rivenworld.game.ai.AiSpawn;
+import com.gamefocal.rivenworld.game.entites.vfx.ShrineFogVFX;
+import com.gamefocal.rivenworld.game.util.Location;
 import com.gamefocal.rivenworld.service.AiService;
+import com.gamefocal.rivenworld.service.PlayerService;
+import com.gamefocal.rivenworld.service.ShrineService;
 
 import java.util.concurrent.TimeUnit;
 
--- RivenWorldServer/src/main/java/com/gamefocal/rivenworld/service/ShrineService.java
+++ RivenWorldServer/src/main/java/com/gamefocal/rivenworld/service/ShrineService.java
@@ -25,9 +25,5 @@ public class ShrineService implements HiveService<ShrineService> {
         shrineLocations.add(Location.fromString("1497.3593,81069.766,9762.352,0.0,0.0,117.75881"));
         shrineLocations.add(Location.fromString("37429.477,142791.56,12110.899,0.0,0.0,-121.77663"));
         shrineLocations.add(Location.fromString("69469.58,123769.945,24625.844,0.0,0.0,-65.81827"));
-
-        for (ShrineFogVFX e : DedicatedServer.instance.getWorld().getEntitesOfType(ShrineFogVFX.class)) {
-            DedicatedServer.instance.getWorld().despawn(e.uuid);
-        }
     }
 }
