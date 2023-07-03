3e98f35fe8bc574e7c7e54b6922959a22137857a
Zackary Pedersen <zackary@snaju.com>
2023-07-02 22:17:45 -0500

    Updated version number

--- RivenWorldServer/src/main/java/com/gamefocal/rivenworld/DedicatedServer.java
+++ RivenWorldServer/src/main/java/com/gamefocal/rivenworld/DedicatedServer.java
@@ -61,8 +61,8 @@ import java.util.concurrent.TimeUnit;
 
 public class DedicatedServer implements InjectionRoot {
 
-    public static final float serverVersion = 1.039f;
-    public static final String serverMinorVersion = "rc6";
+    public static final float serverVersion = 1.04f;
+    public static final String serverMinorVersion = "rc1";
     public static boolean isRunning = true;
     public static DedicatedServer instance;
     public static Gson gson;
